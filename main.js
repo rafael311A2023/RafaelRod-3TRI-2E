316b20f219abdbe069416bc4433f5e2b76509882
Rafael Rodrigues <141244047+rafael311A2023@users.noreply.github.com>
2024-10-28 08:52:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function criaCartao(categoria,pergunta,resposta){
-    console.log(categoria,pergunta,resposta)
+    let container = document.getElementById('container')
+    let article = document.createElement('article')
+    article.className = 'cartao'
+    article.innerHTML = 'isso é um teste'
+    container.appendChild(article)
+
 }
 
 criaCartao('Biologia','Qual a função da Mitocondria?','produção de energia')
