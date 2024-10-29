862aa6d595d1d29772dfca9cf3c16136d0250189
Rafael Rodrigues <141244047+rafael311A2023@users.noreply.github.com>
2024-10-29 08:51:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,21 @@ function criaCartao(categoria,pergunta,resposta){
     let container = document.getElementById('container')
     let article = document.createElement('article')
     article.className = 'cartao'
-    article.innerHTML = 'isso é um teste'
+    article.innerHTML = `
+    <div class="cartao-conteudo">
+    <h3> ${categoria} </h3>
+    <div class="cartao-conteudo-pergunta">
+    <p>${pergunta}</p>
+    </div>
+    <div class="cartao-conteudo-resposta">
+    <p> ${resposta}</p>
+    </div>
+    </div>
+    
+    `
     container.appendChild(article)
 
 }
 
 criaCartao('Biologia','Qual a função da Mitocondria?','produção de energia')
+criaCartao("Português","Oque é um verbo?","É uma palavra que indica acontecimentos representados no tempo")
